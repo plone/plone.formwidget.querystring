ee8dfe7c96e698be8eb6ebd9910554c6e7bce7d2
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:55:31 +0100

    Fixed classifiers in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone : 5.0",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
     ],
